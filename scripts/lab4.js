074ded128b55d02b8097f46246acea784789ccc5
cr1nf <jehermosura1@gmail.com>
2026-04-28 13:31:19 -0700

    Update lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -27,7 +27,7 @@ function sumValues(num1, num2, add) {
 function discountPrices(prices, discount) {
     const discounted = []
     const length = prices.length;
-    if(!length || !Number.isFinite(discount)){
+    if(!length || !Number.isFinite(discount)|| typeof prices === "string"){
         return false;
     }
     let discountedPrice = 0
